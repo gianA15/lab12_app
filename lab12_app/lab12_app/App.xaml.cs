77066318b22b11b6aff37613bd4eab98c7c59eb8
Alumno <Alumno@LAB1509-06>
2023-06-16 16:24:41 -0500

    Agregar archivos de proyecto.

---
+++ lab12_app/lab12_app/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using lab12_app.View;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace lab12_app
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new Page1();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ lab12_app/lab12_app/ViewModel/OperacionesViewModel.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace lab12_app.ViewModel
+{
+    public class OperacionesViewModel : ViewModelBase
+    {
+        int valorA;
+        public int ValorA
+        {
+            get { return valorA; }
+            set
+            {
+                if (valorA != value)
+                {
+                    valorA = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int valorB;
+        public int ValorB
+        {
+            get { return valorB; }
+            set
+            {
+                if (valorB != value)
+                {
+                    valorB = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+        int resultadoArea;
+        public int ResultadoArea
+        {
+            get { return resultadoArea; }
+            set
+            {
+                if (resultadoArea != value)
+                {
+                    resultadoArea = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        int resultadoPerimetro;
+        public int ResultadoPerimetro
+        {
+            get { return resultadoPerimetro; }
+            set
+            {
+                if (resultadoPerimetro != value)
+                {
+                    resultadoPerimetro = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+
+
+
+        public ICommand CalcularArea { get; set; }
+        public ICommand CalcularPerimetro { get; set; }
+
+
+        public OperacionesViewModel()
+        {
+
+            CalcularArea = new Command(() =>
+            {
+                ResultadoArea = ValorA * ValorB;
+            });
+            CalcularPerimetro = new Command(() =>
+            {
+                ResultadoPerimetro = 2 * (ValorA + ValorB);
+            });
+        }
+    }
+}
